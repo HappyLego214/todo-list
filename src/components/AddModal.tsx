4b67300e3627e234c38b18df6d09dcdf15f1bb47
josh <anthony21josh@gmail.com>
2025-01-11 15:31:08 +0800

    add enter and exit functions for AddModal card

--- src/components/AddModal.tsx
+++ src/components/AddModal.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import { AddModalProps } from "./Interfaces";
 
 const AddModal: React.FC<AddModalProps> = ({ setModal }) => {
+  function handleOverlayClick() {
+    setModal(false);
+  }
+
+  function handleContentCLick(e: React.MouseEvent<HTMLDivElement>) {
+    e.stopPropagation();
+  }
+
   return (
-    <div className="modal-overlay">
-      <div className="modal-content"></div>
+    <div className="modal-overlay" onClick={handleOverlayClick}>
+      <div className="modal-content" onClick={handleContentCLick}></div>
     </div>
   );
 };
